6180fd6a62629efec47e4253595a5ec49c098790
Dynamiccreeper <74767888+Dynamiccreeper@users.noreply.github.com>
2022-03-31 14:01:00 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,88 +1,55 @@
-import csv 
-rows=[]
-with open("total_stars.csv","r") as f:
-  csvreader=csv.reader(f)
-  for row in csvreader:
-    rows.append(row)
+import csv
+import pandas as pd
 
-headers=rows[0]
-planet_data_rows=rows[1:]
-print(headers)
-print(planet_data_rows[0])
-headers[0]="row_num"
+rows = []
 
-solor_system_planet_count={}
-for planet_data in planet_data_rows:
-  if solor_system_planet_count.get(planet_data[0]):
-    solor_system_planet_count[planet_data[0]]+=1
-  else:
-    solor_system_planet_count[planet_data[0]]=1
+with open('stars.csv', 'r') as f:
+    csv_r = csv.reader(f)
+    for i in csv_r:
+        rows.append(i)
 
-max_solor_system=max(solor_system_planet_count,key=solor_system_planet_count.get)
-print("solor system {} has maximum planets {}".format(max_solor_system,solor_system_planet_count[max_solor_system]))
+headers = rows[0]
+stars_data = rows[1:]
+headers[0] = 'Index'
 
-koi_351_planets=[]
-for planet_data in planet_data_rows:
-  if max_solor_system==planet_data[11]:
-    koi_351_planets.append(planet_data)
-print(len(koi_351_planets))
-print(koi_351_planets)
+star_data = []
 
-temp_planet_data_rows=list(planet_data_rows)
-for planet_data in temp_planet_data_rows: 
-  planet_mass = planet_data[3] 
-  if planet_mass.lower() == "unknown": 
-    planet_data_rows.remove(planet_data) 
-    continue 
-  else: 
-    planet_mass_value = planet_mass.split(" ")[0] 
-    planet_mass_ref = planet_mass.split(" ")[1] 
-    if planet_mass_ref == "Jupiters": 
-      planet_mass_value = float(planet_mass_value) * 317.8 
-    planet_data[3] = planet_mass_value
-  planet_radius = planet_data[7] 
-  if planet_radius.lower() == "unknown": 
-      planet_data_rows.remove(planet_data) 
-      continue 
-  else: 
-    planet_radius_value = planet_radius.split(" ")[0] 
-    planet_radius_ref = planet_radius.split(" ")[2] 
-    if planet_radius_ref == "Jupiter":
-      planet_radius_value = float(planet_radius_value) * 11.2 
-    planet_data[7] = planet_radius_value
-print(len(planet_data_rows))
+for star in stars_data:
+    if star[3] != '?': 
+        star[3] = float(star[3].strip("\'")) * 1.989e+30
+    
+    if star[4] != '?':
+        star[4] = float(star[4].strip("\'")) * 6.957e+8
 
-import plotly.express as px
-koi_351_planet_masses=[]
-koi_351_planet_names=[]
-for planet_data in koi_351_planets:
-  koi_351_planet_masses.append(planet_data[3])
-  koi_351_planet_names.append(planet_data[1])
-koi_351_planet_masses.append(1)
-koi_351_planet_names.append("earth")
-fig=px.bar(x=koi_351_planet_names,y=koi_351_planet_masses)
-fig.show()
+    star_data.append(star)
 
-planet_masses = []
-planet_radiuses = []
-planet_names = [] 
-for planet_data in planet_data_rows:
-  planet_masses.append(planet_data [3]) 
-  planet_radiuses.append(planet_data[7]) 
-  planet_names.append(planet_data[1]) 
-planet_gravity = [] 
-for index, name in enumerate(planet_names): 
-  gravity = (float(planet_masses[index])*5.972e+24) / (float(planet_radiuses[index])*float(planet_radiuses[index])*6371000*6371000) * 6.674e-11 
-  planet_gravity.append(gravity)
-temp_planet_data_rows = list(planet_data_rows) 
-for planet_data in temp_planet_data_rows:
-  if planet_data[1].lower() == "hd 100546 b": 
-    planet_data_rows.remove(planet_data)
-fig = px.scatter(x=planet_radiuses, y=planet_masses, size=planet_gravity, hover_data=[planet_names]) 
-fig.show()
-low_gravity_planets = [] 
-for index, gravity in enumerate(planet_gravity): 
-  if gravity < 10: 
-    low_gravity_planets.append(planet_data_rows[index]) 
-print(len(low_gravity_planets))
-print(len(planet_data_rows))
+star_data_gravity = []
+
+for star in star_data:
+    if star[3] != '?' and star[4] != '?':
+        gravity = (6.674e-11 * float(star[3]))/(float(star[4])*float(star[4]))
+    star.append(gravity)
+    star_data_gravity.append(star)
+
+name = []
+distance = []
+mass = []
+radius = []
+gravity = []
+
+for i in star_data_gravity:
+    name.append(i[1])
+    distance.append(i[2])
+    mass.append(i[3])
+    radius.append(i[4])
+    gravity.append(i[5])
+
+for i in star_data_gravity:
+    print(i)
+
+df = pd.DataFrame(
+    list(zip(name, distance, mass, radius, gravity)),
+    columns=["Star Name", "Distance", "Mass", "Radius", "Gravity"],
+)
+
+df.to_csv('final.csv')
